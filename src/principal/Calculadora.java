ebeaa24ef7053bfdab7846f5fa31474a4e7640a5
irakaslea <irakaslea@DESKTOP-53U6VQ4>
2022-03-22 12:50:04 +0100

    formato consola

--- src/principal/Calculadora.java
+++ src/principal/Calculadora.java
@@ -6,6 +6,7 @@ import java.util.logging.FileHandler;
 import java.util.logging.Handler;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import java.util.logging.SimpleFormatter;
 
 import logs.LogConsolaMasFichero;
 import menu.Menu;
@@ -26,21 +27,26 @@ public class Calculadora {
 	public static void main(String[] args) {
 
 		int resultado = 0;
-		String operacion = "";
-
 		int[] operandos = new int[2];
 
-		Menu menu = new Menu();
+		String operacion = "";
 
+		Menu menu = new Menu();
 		Operaciones operaciones = new Operaciones();
+
+		// Logger Creacion //
 		Handler consoleHandler = null;
 		Handler fileHandler = null;
 
 		try {
+
 			consoleHandler = new ConsoleHandler();
 			fileHandler = new FileHandler("./LOGS/operaciones.log");
 
 			System.out.println(LOGGER.getHandlers().length);
+
+			// Formato Consola //
+			fileHandler.setFormatter(new SimpleFormatter());
 
 			LOGGER.addHandler(consoleHandler);
 			LOGGER.addHandler(fileHandler);
@@ -53,11 +59,14 @@ public class Calculadora {
 
 			// Eliminamos handler de la consola
 			LOGGER.removeHandler(consoleHandler);
-
 			LOGGER.log(Level.FINE, "Nivel de log cambiado a FINE");
 
-		} catch (IOException exception) {
+		}
+
+		catch (IOException exception) {
+
 			LOGGER.log(Level.SEVERE, "Ocurrio un error en FileHandler.", exception);
+
 		}
 
 		LOGGER.finer("Ejemplo con log FINE en LOGGER completado.");
@@ -104,11 +113,17 @@ public class Calculadora {
 					System.out.println("Operación no válida");
 				}
 
+				// Logger Level Fine //
 				LOGGER.log(Level.FINE, " operando 1 " + operandos[0] + " operando 2 ", +operandos[1]);
-			} catch (ArithmeticException e) {
 
-				System.out.println("no puedes divir entre cero " + e.getMessage());
+			}
+
+			catch (ArithmeticException e) {
+
+				// Logger Level Warning //
 				LOGGER.log(Level.WARNING, "no puedes dividir entre cero ", e);
+				System.out.println("no puedes divir entre cero " + e.getMessage());
+
 			}
 
 		} while (menu.repetir());
